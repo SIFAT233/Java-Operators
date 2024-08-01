0a38e305a0d15b992f4b4475319a12ce84b105c8
S.M Mahmudur Rahman SIfat <mahmudurrahmansifat6@gmail.com>
2024-08-01 23:34:48 +0600

    Add files via upload

---
+++ Q6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Q6 {
+    public static void main(String args[])
+    {
+       Scanner src=new Scanner(System.in);
+       float a=src.nextFloat();
+       float b=src.nextFloat();
+       float x=a;
+       x*=b;
+       float y=a;
+       y/=b;
+       System.out.println("Incremented Value:" +x);
+       System.out.println("Decremented Value:" +y);
+    }
+}
